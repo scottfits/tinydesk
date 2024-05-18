23cef8069ff139ccf8d3635ef20e1264b0ea81cb
scottfits <iscottonline@gmail.com>
2024-05-18 09:58:15 -0700

    Initial Commit

---
+++ OpenDesk/OpenDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenDeskApp.swift
+//  OpenDesk
+//
+//  Created by Scott Fitsimones on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
